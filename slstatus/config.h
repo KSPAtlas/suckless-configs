48306cbd5a2c7556077fc6fbf5b6d65dc78f8f5e
KSPAtlas <72895702+KSPAtlas@users.noreply.github.com>
2021-06-06 16:06:17 +0100

    Add files via upload

--- slstatus/config.h
+++ slstatus/config.h
@@ -65,5 +65,8 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	{ hostname, "  %s  ",		 NULL },
+	{ username, "|  %s  ",		 NULL },
+	{ ram_used, "|  %s  ",		 NULL },
+	{ datetime, "|  %s ",           "%F %T" },
 };
